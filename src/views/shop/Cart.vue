33d502d88345242a0c917d0629e94caf33636490
L <wowuhuaihuai@163.com>
2021-04-15 18:39:38 +0800

    店铺详情-购物车商品数量与价格联动

--- src/views/shop/Cart.vue
+++ src/views/shop/Cart.vue
@@ -3,11 +3,11 @@
     <div class="check">
       <div class="check__icon">
         <img class="check__icon__img" src="http://www.dell-lee.com/imgs/vue3/basket.png" />
-        <div class="check__icon__tag">1</div>
+        <div class="check__icon__tag">{{ total }}</div>
       </div>
       <div class="check__info">
         总计：
-        <span class="check__info__price">&yen;128</span>
+        <span class="check__info__price">&yen; {{ price }}</span>
       </div>
       <div class="check__btn">去结算</div>
     </div>
@@ -15,7 +15,60 @@
 </template>
 
 <script>
-export default {}
+import { computed } from 'vue'
+import { useStore } from 'vuex'
+import { useRoute } from 'vue-router'
+
+const useCartEffect = () => {
+  // vuex 获取购物车数据
+  const store = useStore()
+  const cartList = store.state.cartList
+
+  // vue-router 获取店铺id
+  const route = useRoute()
+  const shopId = route.params.id
+
+  // vue 计算属性 商品数量
+  const total = computed(() => {
+    // 根据购物车数据和店铺id获取购物车中店铺的商品信息
+    const productList = cartList[shopId]
+    let count = 0
+    // 没有商品信息，商品数量默认wi0
+    if (!productList) {
+      return 0
+    }
+    // 循环变量计算商品数量
+    for (const i in productList) {
+      count += productList[i].count
+    }
+    return count
+  })
+
+  // vue 计算属性 商品总价
+  const price = computed(() => {
+    // 根据购物车数据和店铺id获取购物车中店铺的商品信息
+    const productList = cartList[shopId]
+    let price = 0
+    if (!productList) {
+      return 0
+    }
+    // 循环变量计算商品总价
+    for (const i in productList) {
+      price += productList[i].count * productList[i].price
+    }
+    // 保留小数点后两位
+    return price.toFixed(2)
+  })
+  return { total, price }
+}
+
+export default {
+  name: 'Cart',
+  setup() {
+    const { total, price } = useCartEffect()
+    return { total, price }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
@@ -42,7 +95,7 @@ export default {}
       margin: 0.12rem auto;
     }
     &__tag {
-      width: 0.2rem;
+      // width: 0.2rem;
       height: 0.2rem;
       background: $hightlight-fontColor;
       color: $bgColor;
@@ -51,8 +104,10 @@ export default {}
       text-align: center;
       line-height: 0.2rem;
       transform: scale(0.5);
+      transform-origin: left center;
       position: absolute;
-      right: 0.2rem;
+      padding: 0 0.05rem;
+      left: 0.5rem;
       top: 0.04rem;
     }
   }
@@ -63,7 +118,7 @@ export default {}
     color: $content-fontColor;
     line-height: 0.49rem;
     &__price {
-      font-size: .18rem;
+      font-size: 0.18rem;
       color: $hightlight-fontColor;
       position: absolute;
     }
@@ -71,7 +126,7 @@ export default {}
   &__btn {
     width: 0.98rem;
     line-height: 0.49rem;
-    font-size: .14rem;
+    font-size: 0.14rem;
     color: $bgColor;
     background: #4fb0f9;
     text-align: center;
