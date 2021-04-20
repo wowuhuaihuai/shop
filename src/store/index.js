705edf9732a17b400834222d3dc4a931b0cb5c4b
L <wowuhuaihuai@163.com>
2021-04-20 17:04:47 +0800

    购物车组件中全选逻辑-注释代码

--- src/store/index.js
+++ src/store/index.js
@@ -36,27 +36,40 @@ export default createStore({
       // 再把店铺详情 shopInfo 赋值给 state.cartList[shopId]
       state.cartList[shopId] = shopInfo
     },
+    // 改变单个商品的选中状态
     changeCartItemChecked(state, payload) {
-      console.log(payload)
+      // 解构传过来的店铺id和商品id
       const { shopId, productId } = payload
+      // 获取商品item
       const product = state.cartList[shopId][productId]
+      // 把该商品的选中状态取反
       product.check = !product.check
     },
+    // 清空购物车
     cleanCartProducts(state, payload) {
+      // 解构传过来的店铺id
       const { shopId } = payload
+      // 把储存的整个商品的信息置为空字典
       state.cartList[shopId] = {}
     },
+    // 全选/全不选
     setCartItmesChecked(state, payload) {
+      // 解构传过来的店铺id和全选的状态
       const { shopId, allCheckedValue } = payload
-      console.log('allCheckedValue', allCheckedValue)
+      // 获取店铺信息
       const products = state.cartList[shopId]
+      // 保证该店铺有商品
       if (products) {
+        // 如果此时已经全选了
         if (allCheckedValue) {
+          // 就把该店铺的所有商品置为不选中
           for (const key in products) {
             const product = products[key]
             product.check = false
           }
         } else {
+          // 如果此时已经有没有被选了
+          // 就把该店铺的所有商品置为选中
           for (const key in products) {
             const product = products[key]
             product.check = true
