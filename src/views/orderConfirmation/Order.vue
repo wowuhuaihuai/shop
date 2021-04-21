ec65e31ed9a255fa2d72fa7cc3c7d70b387d5d96
L <wowuhuaihuai@163.com>
2021-04-21 20:36:33 +0800

    订单页_支付蒙层

--- src/views/orderConfirmation/Order.vue
+++ src/views/orderConfirmation/Order.vue
@@ -3,6 +3,16 @@
     <div class="order__price">实付金额 &yen;{{ calculations.price }}</div>
     <div class="order__btn">提交订单</div>
   </div>
+  <div class="musk">
+    <div class="musk__content">
+      <div class="musk__content__title">确认要离开收银台？</div>
+      <div class="musk__content__desc">请尽快完成支付，否则将被取消</div>
+      <div class="musk__content__btn">
+        <div class="musk__content__btn--first" @click="handleCancle">取消订单</div>
+        <div class="musk__content__btn--last" @click="handleConfirm">确认支付</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -15,7 +25,13 @@ export default {
     const route = useRoute()
     const shopId = route.params.id
     const { calculations } = useCommonCartEffect(shopId)
-    return { calculations }
+    const handleCancle = () => {
+      alert('取消支付')
+    }
+    const handleConfirm = () => {
+      alert('确认支付')
+    }
+    return { calculations, handleCancle, handleConfirm }
   }
 }
 </script>
@@ -45,6 +61,61 @@ export default {
     font-size: 14px;
     color: $bgColor;
     text-align: center;
+  }
+}
+.musk {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: rgba(0, 0, 0, 0.5);
+  &__content {
+    width: 3rem;
+    height: 1.56rem;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background: #ffffff;
+    border-radius: 4px;
+    text-align: center;
+    &__title {
+      line-height: 0.25rem;
+      font-size: 0.18rem;
+      color: #333333;
+      margin-top: 0.24rem;
+    }
+    &__desc {
+      line-height: 0.2rem;
+      font-size: 14px;
+      color: #666666;
+      margin-top: 0.08rem;
+    }
+    &__btn {
+      margin-top: 0.24rem;
+      line-height: 0.32rem;
+      &--first {
+        width: 0.8rem;
+        display: inline-block;
+        font-size: 14px;
+        color: #0091ff;
+        border: 1px solid #4fb0f9;
+        border-radius: 16px;
+        margin-right: 0.12rem;
+      }
+      &--last {
+        width: 0.8rem;
+        display: inline-block;
+
+        background: #4fb0f9;
+        border: 1px solid #4fb0f9;
+        border-radius: 16px;
+        font-size: 14px;
+        color: #ffffff;
+        margin-left: 0.12rem;
+      }
+    }
   }
 }
 </style>
